e6ede665f11e6d1d8bca9753482595af044fc6ea
Raghulskr12 <kalairaghul70@gmail.com>
2026-06-22 14:45:28 +0530

    [ADD] /games end point added

--- GameStoreAPI/Program.cs
+++ GameStoreAPI/Program.cs
@@ -1,20 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
+using System.Collections.Generic;
+using GameStore.Api.Dtos;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+List<GameDto> games = new List<GameDto>
 {
-    app.MapOpenApi();
-}
+    new GameDto(1, "The Legend of Zelda: Breath of the Wild", "Action-Adventure", 59.99m, new DateOnly(2017, 3, 3)),
+    new GameDto(2, "God of War", "Action-Adventure", 49.99m, new DateOnly(2018, 4, 20)),
+    new GameDto(3, "Red Dead Redemption 2", "Action-Adventure", 39.99m, new DateOnly(2018, 10, 26)),
+    new GameDto(4, "The Witcher 3: Wild Hunt", "RPG", 29.99m, new DateOnly(2015, 5, 19)),
+    new GameDto(5, "Cyberpunk 2077", "RPG", 59.99m, new DateOnly(2020, 12, 10))
+};
 
-app.UseHttpsRedirection();
+app.MapGet("/", () => "GameStoreAPI");
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/games", () => games);
 
 app.Run();
 
